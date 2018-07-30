1d5cce509ae5e456055a62d45f84be3c7979e49c
madddanz <maddiedanzberger@gmail.com>
2018-07-30 16:40:52 +0000

    automatically backed up by learn

--- spec/first_html_tag_lab_spec.rb
+++ spec/first_html_tag_lab_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe 'Your First HTML Tag' do
   it 'is an H1 in index.html' do
     h1 = parsed_html.search('h1').first
-
+<h1> Hello, World! </h1>
     expect(h1).to_not be_nil, hint(1, h1)
   end
 
